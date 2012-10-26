d2697c67eba672b9904b3b378da0bf8571dc2fd9
Antonio Hernández <ahdiaz@gmail.com>
2012-10-26 20:03:00 +0200

    Change the window background.

--- src/ami-desktop.h
+++ src/ami-desktop.h
@@ -2,8 +2,6 @@
  * ${license}
  **/
 
-#include <gdk/gdkx.h>
-#include <gtk/gtk.h>
 
 typedef struct AmiDesktop AmiDesktop;
 
@@ -12,3 +10,5 @@ AmiDesktop *ami_desktop_new(GdkScreen *gscreen);
 void ami_desktop_free(AmiDesktop *desktop);
 
 void ami_desktop_init(AmiDesktop *desktop);
+
+void ami_desktop_set_background(AmiDesktop *desktop);
